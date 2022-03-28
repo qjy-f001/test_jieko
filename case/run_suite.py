faa5f9c287a9144cdebe5929e78ec49526d83f33
ftw001 <fantianwang@qianjiayue.com>
2022-03-28 19:08:37 +0800

    0328

--- case/run_suite.py
+++ case/run_suite.py
@@ -1,19 +1,39 @@
 # 导包
 import unittest
-from tool.HTMLTestRunner import HTMLTestRunner
-# from tool.HTMLTestReportCN import HTMLTestRunner
+# from tool.HTMLTestRunner import HTMLTestRunner  # 英文
+# from tool.HTMLTestReportCN import HTMLTestRunner  # 中文
+from tool.HTMLTestRunnerCN import HTMLTestReportCN  # 中文
+import time
 
-# 组装测试套件
-suite = unittest.defaultTestLoader.discover("./")
+# 生成测试套件
+suite = unittest.defaultTestLoader.discover('./')
 
+# 设置报告存放路径及文件名
+now_time = time.strftime('%Y%m%d_%H%M%S')  # 获取时间戳, 防止报告重名被覆盖
+report_name = '../report/report{}.html'.format(now_time)
 
-unittest.TextTestRunner().run(suite)
+# 打开报告写入文件流
+# 注意: wb 以二进制形式写入内容到文件
+with open(report_name, 'wb') as f:
+    # 初始化执行对象
+    """
+    可选参数含义
+    verbosity: 控制台输出结果信息的模式.默认 1:简要模式, 可选 2:详细模式
+    title:生成的报告文件的内容标题
+    description:测试环境的描述信息
+    tester:测试人员信息(中文模板专有)
+    """
+    # 英文模板
+    # runner = HTMLTestRunner(stream=f,
+    #                         verbosity=2,
+    #                         title='Web 自动化测试报告',
+    #                         description='系统:macOS 浏览器:谷歌 编程语言:Python')
 
-
-# # 使用文本执行测试套件
-# with open("../report/report.txt", "w", encoding="utf-8") as f:
-#     unittest.TextTestRunner(stream=f, verbosity=2).run(suite)
-#
-# # 生成html报告
-# with open("../report/report.html", "wb") as f:
-#     HTMLTestRunner(stream=f, verbosity=2).run(suite)
+    # 中文模板
+    runner = HTMLTestReportCN(stream=f,
+                              verbosity=2,
+                              title='Web 自动化测试报告',
+                              description='系统:Windows 版本:3.9 编程语言:Python',
+                              tester='QA')
+    # 调用执行方法生成测试报告
+    runner.run(suite)
